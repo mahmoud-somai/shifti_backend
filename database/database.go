5168398d8d35895d2b3538eccef415ab449eb06e
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-04-01 11:33:11 +0100

    Configurate environment variables

--- database/database.go
+++ database/database.go
@@ -1,8 +1,34 @@
+// package database
+
+// import (
+// 	"log"
+
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// )
+
+// var DB *gorm.DB
+
+// func ConnectToDB() {
+// 	var err error
+
+// 	dsn := "root:@tcp(127.0.0.1:3306)/testwoo?charset=utf8mb4&parseTime=True&loc=Local"
+// 	//dsn := "admin:helloadmin@tcp(mydb.cj400ku4wauc.us-east-1.rds.amazonaws.com:3306)/wootest?charset=utf8mb4&parseTime=True&loc=Local"
+// 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+// 	if err != nil {
+// 		log.Fatal("Failed to connect DB")
+// 	}
+// }
+
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -12,10 +38,23 @@ var DB *gorm.DB
 func ConnectToDB() {
 	var err error
 
-	dsn := "root:@tcp(127.0.0.1:3306)/testwoo?charset=utf8mb4&parseTime=True&loc=Local"
-	//dsn := "admin:helloadmin@tcp(mydb.cj400ku4wauc.us-east-1.rds.amazonaws.com:3306)/wootest?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// Load environment variables from .env file
+	err = godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
+	// Construct DSN using environment variables
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	// Open database connection
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect DB")
 	}
--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module goback
 go 1.22.0
 
 require (
+	github.com/gin-contrib/cors v1.7.0
+	gorm.io/driver/mysql v1.5.4
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/bytedance/sonic v1.11.3 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
-	github.com/gin-contrib/cors v1.7.0
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.9.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -18,6 +22,7 @@ require (
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -35,6 +40,5 @@ require (
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/driver/mysql v1.5.4
 	gorm.io/gorm v1.25.7 // indirect
 )
